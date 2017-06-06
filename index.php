7152266ffe95d8ce08c9ab9a4bd20a8c0e979aaf
Ahmed B. Qureshi <ahmedbq1993@gmail.com>
2017-06-06 14:26:08 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+	<meta name="viewport" content="device-width, initial-scale=1">
+	<title>Ahmed B. Qureshi</title>
+	<meta charset = "utf-8">
+	<link  rel = "stylesheet" type = "text/css" href = "bootstrap.css">
+	<link  rel = "stylesheet" type = "text/css" href = "normalize.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src = "bootstrap.min.js"></script>
+</head>
+
+<body>
+	<div class = "container-fluid">
+		<img class = "img-fluid" src= "NamePlate.jpg" />
+	</div>
+
+	<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" 
+		aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<a class="navbar-brand" href="index.php"><h3>Developer</h3></a>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+			<!-- 4329 -->
+				<a id = "GithubLogo" class="nav-item nav-link" href="https://github.com/ahmedbq"><span class="sr-only">(current)</span></a>
+				<a id = "LinkedInLogo" class="nav-item nav-link" href="https://www.linkedin.com/in/ahmed-qureshi-765060105/"></a>
+			</div>
+		</div>
+	</nav>
+	
+	<div id = "AboutMe" class = "container">
+		
+		<p>
+		<br>
+		When I applied for biology, I did not expect that 
+		down the line I would be getting a degree in computer science.
+		Although I achieved my biology degree already, I am looking forward
+		to my second bachelor's in computer science in Fall 2017. My biggest
+		hobby is to actually choose which hobby to occupy myself in. One of
+		my biggest ones is to actually teach myself via online material.
+		At the moment my interests are leaning towards learning the SAP Enterprise
+		applications and Apache Hadoop to be a software developer in either one,
+		or to be a full-stack developer. 
+		</p>
+		<br>
+		
+			<table id = "DescTable" class = "table-responsive table-hover table-bordered">
+				<tr>
+					<td>
+						<strong>Language:</strong>
+					</td>
+					<td>
+						Java, C++, JavaScript, PHP, SQL
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<strong>Learning:</strong>
+					</td>
+					<td>
+						Python, Linux
+					</td>
+				</tr>
+				<tr>
+					<td> 
+						<strong>Interests:</strong>
+					</td>
+					<td>
+						Big Data, Algorithms
+					</td>
+				</tr>
+				<tr>
+					<td> 
+						<strong>Contact:</strong>
+					</td>
+					<td>
+						ahmedbq@hotmail.com
+					</td>
+				</tr>
+				<tr>
+					<td colspan = 2>
+						<a id = "DownloadButtonLogo" class="nav-item nav-link" href="Resume.pdf" download="newfilename">
+						<br><br><br><br>Resume</a>
+					</td>
+				</tr>
+			</table>
+
+	</div>
+
+
+</body>
+
+</html>
